50a529a4934ac911f76b7b1da9a5a84ba4543871
antimonio124 <gfniscool@gmail.com>
2025-08-18 22:31:42 +0200

    adding times_each_character_appears

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from stats import num_of_words
-
+from stats import times_each_character_appears
 
 
 def get_book_text(filepath):
@@ -12,8 +12,9 @@ def get_book_text(filepath):
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
+    dic = times_each_character_appears(book_text)
     print (f"{num_of_words(book_text)} words found in the document")
-
+    print(dic)
 
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -1,2 +1,12 @@
 def num_of_words(book_text):
     return len(book_text.split())
+
+def times_each_character_appears(book_text):
+    dictionary = {}
+    for char in book_text:
+        book_text = book_text.lower()
+        if char in dictionary:
+            dictionary[char] += 1
+        else:
+            dictionary[char] = 1
+    return dictionary
